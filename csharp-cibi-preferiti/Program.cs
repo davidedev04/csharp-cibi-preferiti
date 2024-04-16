cd8de803608015660181b131a1d6fe9de615bc42
Davide Prencipe <davidedev04@gmail.com>
2024-04-16 17:18:18 +0200

    BONUS e aggiunta commenti

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,18 +4,25 @@
     {
         static void Main(string[] args)
         {
+            //creiamo un array di cibi preferiti in ordine di preferenza
             string[] foods = { "pizza", "carbonara", "cioccolato", "torta salata", "carne", "sushi", "pistacchio", "pasta e fagioli", "pasta e rape", "pasta e piselli" };
 
+            //stampiamo la lunghezza della classifica
             Console.WriteLine("La lunghezza dell'elenco è: " + foods.Length);
 
+            //stampiamo tutti i cibi preferiti
             for 
                 (int i = 0; i < foods.Length; i++)
             {
                 Console.WriteLine("- " + foods[i]);
             }
 
+            //anche il cibo preferito e il cibo meno
             Console.WriteLine($"Il mio cibo preferito è: {foods[0]}");
             Console.WriteLine($"Il mio cibo meno preferito è: {foods[9]}");
+
+            //e i cibi di metà classifica
+            Console.WriteLine($"I miei cibi di metà classifica sono: {foods[3]} e {foods[4]}");
 
         }
     }
