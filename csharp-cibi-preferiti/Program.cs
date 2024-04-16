0f0e79f0adaabe9d14e712078b0551cef73c3cfb
Davide Prencipe <davidedev04@gmail.com>
2024-04-16 16:34:58 +0200

    piccola correzione e aggiunta piatti

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,11 +4,12 @@
     {
         static void Main(string[] args)
         {
-            string[] foods = { "pizza", "carbonara", "torta salata", "carne", "sushi" };
+            string[] foods = { "pizza", "carbonara", "cioccolato", "torta salata", "carne", "sushi", "pistacchio", "pasta e fagioli", "pasta e rape", "pasta e piselli" };
 
-            Console.WriteLine("La lunghezza dell'elenco è:" + foods.Length);
+            Console.WriteLine("La lunghezza dell'elenco è: " + foods.Length);
 
-            for (int i = 0; i < foods.Length; i++)
+            for 
+                (int i = 0; i < foods.Length; i++)
             {
                 Console.WriteLine("- " + foods[i]);
             }
