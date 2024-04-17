6f393a2b4dce0c00c385f836aab76598d4334e41
Davide Prencipe <davidedev04@gmail.com>
2024-04-17 12:52:57 +0200

    correzione esercizio

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -14,15 +14,24 @@
             for 
                 (int i = 0; i < foods.Length; i++)
             {
-                Console.WriteLine("- " + foods[i]);
+                Console.WriteLine($"{i + 1}- " + foods[i]);
             }
 
             //anche il cibo preferito e il cibo meno
             Console.WriteLine($"Il mio cibo preferito è: {foods[0]}");
-            Console.WriteLine($"Il mio cibo meno preferito è: {foods[9]}");
+            Console.WriteLine($"Il mio cibo meno preferito è: {foods[foods.Length - 1]}");
+
+            if ( foods.Length % 2 == 1 ) 
+            {
+                Console.WriteLine($"il mio cibo di metà classifica é: {foods[foods.Length / 2]}");
+            } else
+            {
+                Console.WriteLine($"i miei cibi di metà classifica sono: {foods[foods.Length / 2]}");
+                Console.WriteLine($"i miei cibi di metà classifica sono: {foods[(foods.Length - 1) / 2]}");   
+            }
 
             //e i cibi di metà classifica
-            Console.WriteLine($"I miei cibi di metà classifica sono: {foods[3]} e {foods[4]}");
+            //Console.WriteLine($"I miei cibi di metà classifica sono: {foods[3]} e {foods[4]}");
 
         }
     }
